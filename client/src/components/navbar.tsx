dd6057f40fd68632bc4937b44e5c9eac0b04b64c
mdavi89 <83469622+mdavi89@users.noreply.github.com>
2025-02-06 18:38:09 -0500

    committing initial files

---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link for routing
+import "./navbar.css"; // Import the CSS file
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="sidebar">
+        {/* Slim Navbar */}
+        <div className="navbar-slim">
+          <Link to="/">Dashboard</Link> {/* Use Link instead of <a> */}
+          <Link to="/courses">Courses</Link>
+          <Link to="/login">Log In</Link>
+        </div>
+
+        {/* Wide Navbar */}
+        <div className="navbar-wide">
+          <Link to="/syllabus">Syllabus</Link>
+          <Link to="/lessons">Lessons</Link>
+          <Link to="/quizzes">Quizzes</Link>
+          <Link to="/tests">Tests</Link>
+          <Link to="/grades">Grades</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
