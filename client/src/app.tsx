57f36e02348c67ecbc9a2ed5a50e14937678bafb
Marcel Peyton <marcel-p@outlook.com>
2025-02-06 19:20:13 -0500

    login showing

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/navbar"; // Import your Navbar component
 // Create simple components for each route
 import Dashboard from "./pages/dashboard";
 const Courses = () => <h2>Courses Page</h2>;
-const Login = () => <h2>Login Page</h2>;
+import Login from "./pages/Login";
 import Syllabus from "./pages/syllabus";
 const Lessons = () => <h2>Lessons Page</h2>;
 const Quizzes = () => <h2>Quizzes Page</h2>;
