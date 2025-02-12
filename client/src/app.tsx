068656235eb04c408609512d087a8b4da57a778a
mdavi89 <mdavi359@yahoo.com>
2025-02-12 14:10:26 -0500

    Update app.tsx

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -34,7 +34,7 @@ import { setContext } from '@apollo/client/link/context';
 // Construct our main GraphQL API endpoint
 
 const httpLink = createHttpLink({
-  uri: import.meta.GRAPHQL_URI,
+  uri: import.meta.env.GRAPHQL_URI,
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
