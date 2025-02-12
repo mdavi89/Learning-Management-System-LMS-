4c92259c79eae59aebad9264c71a318014c9ee5e
Matthew Davis <mdavi359@yahoo.com>
2025-02-12 13:28:33 -0500

    updating routing

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,15 +1,9 @@
-import { useState } from "react"; // Import useState for managing state
 import { Link } from "react-router-dom"; // Import Link for routing
 import "../styling/navbar.css"; // Import the CSS file
 
 import Auth from '../utils/auth';
 
 const Navbar = () => {
-  const [isDropdownOpen, setDropdownOpen] = useState(false); // State for dropdown visibility
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen); // Toggle dropdown visibility
-  };
 
   return (
     <div className="container">
@@ -18,16 +12,6 @@ const Navbar = () => {
         <div className="navbar-slim">
           <Link to="/">Dashboard</Link> {/* Use Link instead of <a> */}
            <div className="dropdown">           
-
-            <Link to="/courses" onClick={toggleDropdown}>Courses</Link> {/* Toggle dropdown on click */}
-            {isDropdownOpen && (
-              <div className="dropdown-menu">
-                <Link to="/javascriptClass">JavaScript</Link>
-                <Link to="/typescriptClass">TypeScript</Link>
-                <Link to="/pythonClass">Python</Link>
-              </div>
-              
-            )}
             </div>
           {/* If you are already loggedIn, just display user */}
           {Auth.loggedIn() ? (<div> 
